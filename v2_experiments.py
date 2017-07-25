ba8f86ab755ba44d7477a9cb702ade05f11b5ce8
Rune Kaagaard <r@Runes-MacBook-Pro.local>
2017-07-25 15:42:54 +0200

    workflow decorator.

--- v2_experiments.py
+++ v2_experiments.py
@@ -1,14 +1,15 @@
 from collections import namedtuple
+from functools import wraps
 
 
 class InvalidArrowException(Exception):
     pass
 
 
-def run_workflow(func, args, state, error_step=None):
+def run_workflow(func, args, kwargs, state, error_step=None):
     ARROWS = namedtuple('ARROWS', 'continues aborts')("_continues_",
                                                       "_aborts_")
-    for step in func(args):
+    for step in func(*args, **kwargs):
         try:
             state, arrow = step(state, ARROWS)
             if isinstance(arrow, Exception):
@@ -30,6 +31,17 @@ def run_workflow(func, args, state, error_step=None):
     return state
 
 
+def workflow(state, error_step):
+    def _(func):
+        @wraps(func)
+        def __(*args, **kwargs):
+            return run_workflow(func, args, kwargs, state, error_step)
+
+        return __
+
+    return _
+
+
 def reduces(reducer):
     return lambda state, arrows: (reducer(state), arrows.continues)
 
@@ -38,11 +50,28 @@ def aborts():
     return lambda state, arrows: (state, arrows.aborts)
 
 
+def raises(exception):
+    return lambda state, arrows: (state, exception)
+
+
+def twelve_sucks(state, arrows):
+    return state + 1213, arrows.continues
+
+
+def sums_error(exception, state, arrows):
+    return state + 10**6, arrows.continues
+
+
+@workflow(state=0, error_step=sums_error)
 def sums(xs):
     for x in xs:
-        if x > 12:
+        if x == 12:
+            yield twelve_sucks
+            continue
+        if x > 16:
+            yield raises(Exception("Noooo"))
             yield aborts()
         yield reduces(lambda state: state + x)
 
 
-print run_workflow(sums, range(20), 0)
+print sums(range(30))
